20802ad6ba84ca1d88d529084cf3b57e880ecd30
Sanjanarlsb <sanjanabommnalli30@gmail.com>
2026-09-19 11:04:25 +0530

    Adding Largest of 3 program

---
+++ python/problems/largest_of_3.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=int(input("Enter the third number"))
+if a > b:
+    if a > c:
+        print("a is largest")
+    else :
+        print("c is largest")
+elif b > a:
+    if b > c:
+        print("b is largest")
+    else :
+        print("c is largest")
+else :
+    print("c is largest")
