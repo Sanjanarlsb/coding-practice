a6e01a01c8be3a872544d7e13b3b3f49a9a370fb
Sanjanarlsb <sanjanabommnalli30@gmail.com>
2026-09-19 11:01:17 +0530

    Adding Largest of two numbers program

---
+++ python/problems/largest_of_2.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+if a > b:
+    print("a is the greatest number")
+elif b > a:
+    print("b is the greatest number")
+else :
+    print("Both the numbers are equal")
